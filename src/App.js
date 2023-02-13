ddf77d65009ebfdadb9400aeafed421a4a3afd97
MCG <martygleeson@Hotmail.com>
2023-02-13 17:14:50 +1100

    Quick update to app.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          This is an update to the react site, hosted on AWS
         </a>
       </header>
     </div>
